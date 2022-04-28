585205470f9aa8eb27ba23f3246eea12e06be5c5
amnonongus <charmilano@gmail.com>
2022-04-27 22:12:10 -0700

    finished one error to fix

--- dogcollector/main_app/models.py
+++ dogcollector/main_app/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 from django.urls import reverse
 
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.IntegerField()
+    toy = models.ManyToManyField(Toy)
 
     def __str__(self):
-        return self.name
+        return f"The dog named {self.name} has id of {self.id}"
 
 
     def get_absolute_url(self):
--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,7 +1,9 @@
+from xml.etree.ElementTree import tostring
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
 from .forms import WalkForm
 
 class DogCreate(CreateView):
@@ -22,7 +24,7 @@ class DogDelete(DeleteView):
 
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -33,13 +35,37 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
+    toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toy.all().values_list('id'))
     walk_form = WalkForm()
-    return render(request, 'dogs/detail.html', {'dog': dog, 'walk_form': walk_form})
+    return render(request, 'dogs/detail.html', {'dog': dog, 'walk_form': walk_form, 'toys': toys_dog_doesnt_have})
 
 def add_walk(request, dog_id):
     form = WalkForm(request.POST)
     if form.is_valid():
         new_walk = form.save(commit=False)
-        new_walk.dog.id = dog_id
+        new_walk.dog_id = dog_id
         new_walk.save()
     return redirect('detail', dog_id=dog_id)
+
+def assoc_toy(request, dog_id, toy_id):
+    Dog.objects.get(id=dog_id).toy.add(toy_id)
+    return redirect('detail', dog_id=dog_id)
+
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
